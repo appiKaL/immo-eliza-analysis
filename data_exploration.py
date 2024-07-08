bce930b8eae65a64e6bff36228f3fd8d3b28c1c3
Ness <nessgira1@gmail.com>
2024-07-08 11:59:27 +0200

    final data json to csv

--- data_exploration.py
+++ data_exploration.py
@@ -1,11 +1,21 @@
 import json
 import pandas as pd
 
-with open('denis-immoscraper/data/output.json') as file:
+with open('denis-immoscraper/data/final_dataset.json') as file:
     json_data = json.load(file)
     
-df = pd.json_normalize(json_data)
-df.to_csv('output.csv', index=False)
+keys = json_data.keys()
 
-df_csv = pd.read_csv('output.csv')
-df_csv.to_excel('output.xlsx', index=False)
+inner_keys = next(iter(json_data.values())).keys()
+
+data_list = []
+
+for inner_key in inner_keys:
+    entry = {}
+    for key in keys:
+        entry[key] = json_data[key].get(inner_key)
+    data_list.append(entry)
+    
+df = pd.DataFrame(data_list)
+df.to_csv('final_dataset.csv', index=False)
+df.to_excel('final_dataset.xlsx', index=False)
